48260e1e79578542abc167c831e491029f3d682b
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-07-30 11:17:26 -0400

    Add files via upload

---
+++ JavaScript Crash Course/Declaring Variables.js
@@ -0,0 +1,20 @@
+let petDog = "Riley";
+let petCat = "Coco";
+
+console.log(petDog);
+console.log(petCat);
+
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is: " + petCat);
+
+let dogSound = "woof";
+let catSound = "meow";
+
+console.log("My pet dog " + petDog + " says " + dogSound);
+console.log("My pet cat " + petCat + " says " + catSound);
+
+catSound = "purr";
+console.log("My pet cat " + petCat + " now says " + catSound);
+
+
+
